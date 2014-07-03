20af3c5f97d191f6f8f52d63746f4c4f3781f49b
pantau79 <pantau79@googlemail.com>
2014-07-03 12:54:36 +0200

    doing some first changes

--- ex3.c
+++ ex3.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main()
+/* This part could become an information area */
 {
-  int age = 30;
-  int height = 72;
+	int age = 30;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d meters tall.\n", height);
   
-  printf("I am %d years old.\n", age);
-  printf("I am %d meters tall.\n", height);
-  
-  return 0;
+	return 0;
 }
