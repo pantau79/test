c52077a24902ecb59f1c82487359f42a874b6e9b
pantau79 <pantau79@googlemail.com>
2014-07-03 17:10:03 +0200

    doing a fresh start

--- ex3.c
+++ ex3.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main()
-/* This part could become an information area */
+int main(int argc, char *argv[])
+/* There shall come forth a clear input argument handler with parameters maybe */
 {
-	int age = 30;
-	int height = 72;
+	int i = 0;
+	for (i = 0; i < argc; i++) {
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
 
-	printf("I am %d years old.\n", age);
-	printf("I am %d meters tall.\n", height);
-  
+	/* USAGE of the program (if no argument is given) */
+	if (argc == 1) {
+		printf("Usage: ./ex3 [option1] [option2] [value:int] [destination:folder])\n");
+		printf("option1: -s --- secure connection\n");
+		printf("option2: -p:profile_name --- login as given 'profile_name'\n");
+		printf("value: if task requires an int value such as a port number\n");
+		printf("destination: is the unix path to the destination folder\n");
+	}
 	return 0;
 }
-/* There shall come forth a clear input argument handler with parameters maybe */
 
+/* This part could become an information area */
